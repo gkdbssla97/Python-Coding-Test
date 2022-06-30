e3aaec2bb717a4736f8c0cb8b6254dc3d3362f3b
Hayoon <gkdbssla97@gmail.com>
2022-06-30 20:15:38 +0900

    .

--- 소수(에라토스테네스_체).py
+++ 소수(에라토스테네스_체).py
@@ -4,11 +4,11 @@ prime_num = [0] * 200001
 #print(prime_num)
 
 cnt = 0
+k = 0
 for i in range(2, N + 1):
     if prime_num[i] == 0:
         cnt += 1
-        for j in range(i, N + 1, i):
-            #print(j*i)
+        for j in range(i + i, N + 1, i):
             prime_num[j] = 1
 print(cnt)
 
