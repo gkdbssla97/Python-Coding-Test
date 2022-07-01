24a274af38d57ca9efca564e6f7baacbe984bdd1
Hayoon <gkdbssla97@gmail.com>
2022-07-01 21:08:02 +0900

    .

---
+++ 숫자만_추출.py
@@ -0,0 +1,12 @@
+str = input()
+num = ""
+for i in str:
+    if i >= '0' and i <= '9':
+        num += i
+num = int(num)
+print(num)
+cnt = 0
+for i in range(1, num + 1):
+    if num % i == 0:
+        cnt += 1
+print(cnt)
