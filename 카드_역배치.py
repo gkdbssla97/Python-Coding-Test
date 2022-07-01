7b9160e3777a33866ea637d4c1a09881fc6bae9c
Hayoon <gkdbssla97@gmail.com>
2022-07-01 22:16:25 +0900

    .

--- 카드_역배치.py
+++ 카드_역배치.py
@@ -1,4 +1,5 @@
-card = [i for i in range(1, 21)]
+#card = [i for i in range(1, 21)]
+card = list(range(21))
 #print(card)
 '''
 for i in range(1, 11):
@@ -16,15 +17,18 @@ print(card)
 '''
 for i in range(1, 11):
     ai, bi = map(int, input().split())
-    idx = 1
-    for k in range(ai, ((ai + bi) // 2) + 1):
-        print(ai, (bi // 2) + 1, k)
-        card[k - 1], card[bi - idx] = card[bi - idx], card[k - 1]
-        idx += 1
+    #idx = 1
+    # for k in range(ai, ((ai + bi) // 2) + 1):
+    #     print(ai, (bi // 2) + 1, k)
+    #     card[k - 1], card[bi - idx] = card[bi - idx], card[k - 1]
+    #     idx += 1
+    for j in range((bi - ai + 1) // 2):
+        card[ai + j], card[bi - j] = card[bi - j], card[ai + j]
     print(f'#{i}: {card}')
         # 5 6 7 8 9 10
         # 4 5 6 7 8 9 - idx
     #print(f'#{i + 1}: {card}')
+card.pop(0)
 print(card)
 '''
 [19, 1, 2, 4, 3, 5, 8, 7, 6, 9, 15, 16, 17, 12, 11, 10, 14, 13, 18, 20]
