4e5245e99b3d48d2918936c1aac28e0e4eec264f
Hayoon <gkdbssla97@gmail.com>
2022-07-06 10:38:11 +0900

    .

--- 격자판_최대합.py
+++ 격자판_최대합.py
@@ -5,14 +5,43 @@ for i in range(N):
     cube.append(list(map(int, input().split())))
 
 #행
-max = 0
-sum = 0
+row_max = 0
+tmp = 0
 for i in range(N):
-    sum = sum(cube[i])
-    if sum > max:
-        max = sum
-print(max)
+    row = cube[i]
+    tmp = sum(row[0:N])
+    #print(tmp)
+    if tmp > row_max:
+        row_max = tmp
+print(row_max)
+#열
+col_max = 0
+tmp = []
+for i in range(N):
+    col = cube[i]
+    tmp += col[0:N]
+    print(tmp)
+    # if tmp > col_max:
+    #     col_max = tmp
+print(col_max)
 
+cross_max = 0
+for i in range(N):
+    cross1 = cube[i]
+    tmp1 = cross1[i]
+    cross2 = cube[i][N - i - 1]
+    tmp2 = cross2[]
+    print(tmp)
+    # if tmp > col_max:
+    #     col_max = tmp
 
 print(cube)
 
+'''
+5
+10 13 10 12 15 
+12 39 30 23 11 
+11 25 50 53 15 
+19 27 29 37 27 
+19 13 30 13 19
+'''
