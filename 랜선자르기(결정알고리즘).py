fab1ffa27f6105d07275b4afb79be0b75b3fd1f1
Hayoon <gkdbssla97@gmail.com>
2022-07-09 21:13:34 +0900

    .

---
+++ 랜선자르기(결정알고리즘).py
@@ -0,0 +1,35 @@
+K, N = map(int, input().split())
+LAN = []
+for _ in range(K):
+    LAN.append(int(input()))
+max_val = max(LAN)
+
+s = 1
+e = max_val
+while True:
+    sum = 0
+    mid = (s + e) // 2
+    print(f'mid1:{mid} s,e {s} {e}')
+    #print(mid)
+    for i in LAN:
+        sum += i // mid
+    if sum < N:
+        e = mid - 1
+    elif sum >= N:
+        res = mid
+        print(f'mid2:{mid} res:{res}')
+        s = mid + 1
+    if s > e:
+        break
+print(mid)
+#시간초과
+#결정알고리즘(범위 안에 답이 있음을 인지하고 있음)
+#이분탐색으로 문제 접근
+#어떻게 탈출? start > end 일경우 종료
+'''
+4 11
+802
+743
+457 
+539
+'''
---
+++ 뮤직비디오(결정알고리즘).py
@@ -0,0 +1,32 @@
+N, M = map(int, input().split())
+
+MV = list(map(int, input().split()))
+
+s = 1
+#e = sum(MV[M-1:])
+e = sum(MV)
+while True:
+    sum = 0
+    cnt = 1
+    mid = (s + e) // 2
+    for i in MV:
+        if sum + i > mid:
+            cnt += 1
+            sum = i
+        else:
+            sum += i
+    if cnt <= M:
+        res = mid
+        e = mid - 1
+    else:
+        s = mid + 1
+    if s > e:
+        break
+
+print(res)
+
+
+'''
+9 3
+1 2 3 4 5 6 7 8 9
+'''
