f3fd94688d713b22f230e0ea47ee5167781b6e62
Hayoon <gkdbssla97@gmail.com>
2022-07-12 21:53:43 +0900

    solved.

--- 침몰하는_타이타닉(그리디).py
+++ 침몰하는_타이타닉(그리디).py
@@ -1,20 +1,20 @@
-from itertools import combinations
+from collections import deque
 
 N, M = map(int, input().split())
 
 customer = list(map(int, input().split()))
 customer.sort()
-print(customer)
+customer = deque(customer)
 cnt = 0
 #min = 21470000
-while len(customer) != 0:
+while customer:#len(customer) != 0:
     #print(1)
     if len(customer) == 1:
         cnt += 1
         customer.pop()
     elif customer[0] + customer[-1] <= M:
         cnt += 1
-        customer.pop(0)
+        customer.popleft()
         customer.pop()
     else:
         cnt += 1
