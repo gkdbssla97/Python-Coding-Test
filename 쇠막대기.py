01d197dcb1cbe477c853b6515b4e9325f3a778fc
Hayoon <gkdbssla97@gmail.com>
2022-07-17 15:03:34 +0900

    solved.

---
+++ 쇠막대기.py
@@ -0,0 +1,19 @@
+steel = input()
+
+stack = []
+
+cnt = 0
+tmp = ''
+for x in steel:
+    if x == ')' and tmp != ')':
+        stack.pop()
+        cnt += len(stack)
+        print(f'#2:{cnt}')
+    elif x == '(':
+        stack.append(x)
+    elif x == ')' and tmp == ')':
+        stack.pop()
+        cnt += 1
+        print(f'#1:{cnt}')
+    tmp = x
+print(cnt)
