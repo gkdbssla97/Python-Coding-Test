b4a5d8f02dfba09ae2dba126e7daef3d7ec7bfcb
Hayoon <gkdbssla97@gmail.com>
2022-07-17 21:10:10 +0900

    solved.2

--- 쇠막대기.py
+++ 쇠막대기.py
@@ -3,17 +3,27 @@ steel = input()
 stack = []
 
 cnt = 0
-tmp = ''
-for x in steel:
-    if x == ')' and tmp != ')':
-        stack.pop()
-        cnt += len(stack)
-        print(f'#2:{cnt}')
-    elif x == '(':
-        stack.append(x)
-    elif x == ')' and tmp == ')':
-        stack.pop()
-        cnt += 1
-        print(f'#1:{cnt}')
-    tmp = x
+# tmp = ''
+# for x in steel:
+#     if x == ')' and tmp != ')':
+#         stack.pop()
+#         cnt += len(stack)
+#         print(f'#2:{cnt}')
+#     elif x == '(':
+#         stack.append(x)
+#     elif x == ')' and tmp == ')':
+#         stack.pop()
+#         cnt += 1
+#         print(f'#1:{cnt}')
+#     tmp = x
+for i in range(len(steel)):
+    if steel[i] == '(':
+        stack.append(steel[i])
+    else:
+        if steel[i-1] == '(':
+            stack.pop()
+            cnt += len(stack)
+        else:
+            stack.pop()
+            cnt += 1
 print(cnt)
