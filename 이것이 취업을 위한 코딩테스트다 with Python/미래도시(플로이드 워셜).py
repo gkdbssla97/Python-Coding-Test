1a7de5a67836fac55d27e464068708a9b02c3d1a
Hayoon <hy97@sju.ac.kr>
2022-11-23 16:12:21 +0900

    플루이드 워샬 유형

---
+++ 이것이 취업을 위한 코딩테스트다 with Python/미래도시(플로이드 워셜).py
@@ -0,0 +1,46 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+INF = int(1e9)
+graph = [[INF] * (N + 1) for _ in range(N + 1)]
+
+for i in range(N + 1):
+    for j in range(N + 1):
+        if i == j:
+            graph[i][j] = 0
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+for k in range(N + 1):
+    for i in range(N + 1):
+        for j in range(N + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+X, K = map(int, input().split())
+
+print(graph[1][K] + graph[K][X] if graph[1][K] + graph[K][X] != 2 * INF else -1)
+
+'''
+5 7
+1 2
+1 3
+1 4
+2 4
+3 4
+3 5
+4 5
+4 5
+'''
+
+'''
+4 2
+1 3
+2 4
+3 4
+'''
